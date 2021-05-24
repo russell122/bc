59b8500789257e6f26886394db582c8483c663df
russell122 <35368676+russell122@users.noreply.github.com>
2021-05-24 12:11:15 +0300

    Add files via upload

--- web/js/main.js
+++ web/js/main.js
@@ -8,7 +8,8 @@ document.addEventListener('DOMContentLoaded', function () {
         return Array.isArray(v) ? a.concat(f(v)) : a.concat(v);
       }, []);
     }(this);
-  };
+  }; // Imask на мобильный телефон
+
   var telInputs = document.querySelectorAll('input[type="tel"]');
 
   var crateMaskForTel = function crateMaskForTel(inp) {
@@ -86,11 +87,12 @@ document.addEventListener('DOMContentLoaded', function () {
   var $globalHtmlElements = {};
   window.$globalHtmlElements = $globalHtmlElements; // Модальное окно нужное
 
-  $globalHtmlElements.modalFive = "\n\t\t<div class=\"\"></div>\n\t\t<form class=\"modal__form\">\n\t\t\t<div class=\"form__block\">\n\t\t\t\t<h1 class=\"title-1\">\u041E\u0441\u0442\u0430\u0432\u044C\u0442\u0435 \u0441\u0432\u043E\u0438 \u0434\u0430\u043D\u043D\u044B\u0435 \u0438 \u043C\u044B \u0441\u0432\u044F\u0436\u0435\u043C\u0441\u044F \u0441 \u0432\u0430\u043C\u0438 \u0432 \u0431\u043B\u0438\u0436\u0430\u0439\u0448\u0435\u0435 \u0432\u0440\u0435\u043C\u044F</h1>\n\t\t\t\t<div class=\"form__inputs\">\n\t\t\t\t\t<input type=\"text\" class=\"g-input\" placeholder=\"\u0418\u043C\u044F\" required name=\"name\">\n\t\t\t\t\t<input type=\"tel\" class=\"g-input\" placeholder=\"+7 (...) ...  ..  ..\" required name=\"mob\">\n\t\t\t\t\t<input type=\"email\" class=\"g-input\" placeholder=\"E-mail\" required name=\"mail\">\n\t\t\t\t</div>\n\t\t\t<div class=\"form__btns\">\n\t\t\t\t<button type='submit' class=\"button\">\u041E\u0441\u0442\u0430\u0432\u0438\u0442\u044C \u0437\u0430\u044F\u0432\u043A\u0443</button>\n\t\t\t</div>\n\t\t\t</div>\n\t\t</form>\n\t"; // Модальное окно нужное
+  $globalHtmlElements.modalFive = "\n\t\t<form class=\"modal__form\">\n\t\t\t<div class=\"form__block\">\n\t\t\t\t<h1 class=\"title-1\">\u041E\u0441\u0442\u0430\u0432\u044C\u0442\u0435 \u0441\u0432\u043E\u0438 \u0434\u0430\u043D\u043D\u044B\u0435 \u0438 \u043C\u044B \u0441\u0432\u044F\u0436\u0435\u043C\u0441\u044F \u0441 \u0432\u0430\u043C\u0438 \u0432 \u0431\u043B\u0438\u0436\u0430\u0439\u0448\u0435\u0435 \u0432\u0440\u0435\u043C\u044F</h1>\n\t\t\t\t<div class=\"form__inputs\">\n\t\t\t\t\t<input type=\"text\" class=\"g-input\" placeholder=\"\u0418\u043C\u044F\" required name=\"name\">\n\t\t\t\t\t<input type=\"tel\" class=\"g-input\" placeholder=\"+7 (...) ...  ..  ..\" required name=\"mob\">\n\t\t\t\t\t<input type=\"email\" class=\"g-input\" placeholder=\"E-mail\" required name=\"mail\">\n\t\t\t\t</div>\n\t\t\t<div class=\"form__btns\">\n\t\t\t\t<button type='submit' class=\"button\">\u041E\u0441\u0442\u0430\u0432\u0438\u0442\u044C \u0437\u0430\u044F\u0432\u043A\u0443</button>\n\t\t\t</div>\n\t\t\t</div>\n\t\t</form>\n\t"; // Модальное окно нужное
 
   $globalHtmlElements.modalAutoriz = "\n\t\t<form class=\"modal__form2\">\n\t\t\t<div class=\"form__block2\">\n\t\t\t\t<h1 class=\"title-1\">\u0421\u043F\u0430\u0441\u0438\u0431\u043E \u0437\u0430 \u0437\u0430\u044F\u0432\u043A\u0443!</h1>\n\t\t\t\t<div class=\"form__btns\">\n\t\t\t\t\t<a href=\"#\" class=\"button ex\">\u0417\u0430\u043A\u0440\u044B\u0442\u044C</a>\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t</form>\n\t"; // end modal
 });
 $(document).ready(function () {
+  // отправляю данные с формы
   $('.hero__inputs').submit(function (e) {
     e.preventDefault(); // console.log('Есть клик')
 
@@ -106,7 +108,8 @@ $(document).ready(function () {
         console.log('Ошибка');
       }
     });
-  });
+  }); // отправляю данные с формы
+
   $('.price__form').submit(function (e) {
     e.preventDefault();
     var $form = $(this);
